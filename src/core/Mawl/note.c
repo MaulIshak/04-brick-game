f0c982eaefc68897ab9eba334f220e531c48eeae
Maulana Ishak <98244927+MaulIshak@users.noreply.github.com>
2025-02-21 16:16:26 +0700

    feat: implement note initialization and update logic for beatmap handling

--- src/core/Mawl/note.c
+++ src/core/Mawl/note.c
@@ -1,16 +1,27 @@
 #include "raylib.h"
 #include "gameplay.h"
 #include "note.h"
+#include "context.h"
 
-void note_draw(DrawableNote *Note){
-
+void note_draw(DrawableNote *self){
+  if(self->isBeatmapLoaded){
+    // Gambar note disini
+  }
 }
-void note_update(DrawableNote *Note){
-  
+void note_update(DrawableNote *self){
+  if(!self->isBeatmapLoaded){
+    self->beatmap = GetSelectedMusicBeatmap();
+    self->isBeatmapLoaded = true;
+  }
 }
 bool note_isShow(DrawableNote *self){
   if(self->ctx->app_state == APP_PLAYING){
     return true;
   }
   return false;
+}
+
+void InitNote(DrawableNote* self, AppContext *ctx){
+  self->isBeatmapLoaded = false;
+  self->ctx = ctx;
 }
--- src/core/Mawl/note.h
+++ src/core/Mawl/note.h
@@ -9,13 +9,15 @@
 typedef struct {
   // Gameplay *gp;
   AppContext *ctx;
-  Note *note;
-  Texture2D noteTexture[4];
+  Beatmap beatmap;
+  Texture2D noteTexture[NODE_TEXTURE_COUNT];
+  bool isBeatmapLoaded;
 }DrawableNote;
 
 void note_draw(DrawableNote *);
 void note_update(DrawableNote *);
 bool note_isShow(DrawableNote *);
+void InitNote(DrawableNote *, AppContext*);
 
 impl_scene(DrawableNote*, Note_toScene, note_draw, note_update, note_isShow);
 #endif // NOTE_H
