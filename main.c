7788f7388345e1c2b8fca54aba472cbca29a71c8
Maulana Ishak <98244927+MaulIshak@users.noreply.github.com>
2025-03-17 09:03:41 +0700

    Change game size

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ int main()
 
   AppContext ctx = CreateContext(screenWidth, screenHeight);
   // ctx.app_state = APP_BEATMAP_CREATOR;
-  ctx.app_state = APP_LOADING;
+  // ctx.app_state = APP_LOADING;
   Loading loading = {
     .ctx = &ctx
   };
